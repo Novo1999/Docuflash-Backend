75decc95066a04f178595578287c6bbb6c9a089f
Novo1999 <neptune.navo@gmail.com>
2026-05-02 20:30:35 +0600

    feat: added error log

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,8 +1,14 @@
+import { NextFunction, Request, Response } from 'express'
 import { AppError } from '../errors/AppError'
 import createErrorResponse from '../utils/createErrorResponse'
-import { NextFunction, Request, Response } from 'express'
 
 export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(`[${new Date().toISOString()}] ${req.method} ${req.path}`, {
+    name: error.name,
+    message: error.message,
+    stack: error.stack,
+  })
+
   if (error instanceof AppError) {
     return createErrorResponse(res, {
       msg: error.message,
